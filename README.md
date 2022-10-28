6460c44c7409ecb922439e0422b25ba5c5a18a22
dimasrizqi <dimasrizqi3128@yahoo.co.id>
2022-10-29 05:48:34 +0700

    first upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple-Net-Monitoring.1
+
+
+Admin login : .
+User : admin@gmail.com
+Pwd : admin
---
+++ lib/db/dbconfig.php
@@ -0,0 +1,8 @@
+<?php 
+$dbhost = "localhost:3307";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "netmonitoring";
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+ ?>
