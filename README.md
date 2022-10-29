21395c11b78a93a70d3e3fcbb39d27c0cdace85f
dimasrizqi <dimasrizqi3128@yahoo.co.id>
2022-10-29 07:30:03 +0700

    edit dikit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Simple-Net-Monitoring.1
-
+Untuk tutorial penginstalan bisa baca di http://codinger-mini.blogspot.com
+Bug, saran, masukan bisa komen di blog atau ke fb: fb.me/rizal.ofdraw
 
 Admin login : .
 User : admin@gmail.com
--- lib/db/dbconfig.php
+++ lib/db/dbconfig.php
@@ -1,8 +1,8 @@
 <?php 
-$dbhost = "localhost:3307";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "netmonitoring";
+$dbhost = "localhost:3333";
+$dbuser = "eloq4431_webnms";
+$dbpass = "Cemerlang_2022";
+$dbname = "eloq4431_webnms";
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
  ?>
